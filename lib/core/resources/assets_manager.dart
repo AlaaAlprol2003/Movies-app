0d6ade29098e8866186916112a1a6e4123c76dbb
mohamed howedy <howedyy@example.com>
2025-12-01 01:00:48 +0200

    add onboarding screen

--- lib/core/resources/assets_manager.dart
+++ lib/core/resources/assets_manager.dart
@@ -1,3 +1,10 @@
-abstract class AssetsManager{
+abstract class BoardingConstants{
+  static const String base = 'assets/images/';
+  static const String boarding1 = '${base}boarding1.png';
+  static const String boarding2 = '${base}boarding2.png';
+  static const String boarding3 = '${base}boarding3.png';
+  static const String boarding4 = '${base}boarding4.png';
+  static const String boarding5 = '${base}boarding5.png';
+  static const String boarding6 = '${base}boarding6.png';
 
 }
