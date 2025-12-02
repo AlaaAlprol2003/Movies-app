a46919fcfc308c2ff5c18598c1a3493ca3758e59
Alaa Ahmed <alaaalprol@gmail.com>
2025-12-02 23:57:57 +0200

    - add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -7,7 +8,9 @@ import 'package:movies_app/features/auth/presentation/cubit/auth_cubit.dart';
 import 'config/theme_manager.dart';
 import 'core/resources/routes_manager.dart';
 
-void main() {
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   configureDependencies();
   runApp(
     BlocProvider(
