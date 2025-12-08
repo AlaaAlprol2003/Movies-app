6ca0888c4399cef30436feb8d61ffa2e7f4eb2fa
Abdelhady Elraey <abdelhadyelraey411@gmail.com>
2025-12-09 01:40:18 +0200

    l10n

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:movies_app/core/di/service_locator.dart';
 import 'package:movies_app/features/auth/presentation/cubit/auth_cubit.dart';
-import 'package:movies_app/features/movies/presentation/main_layout/main_layout_provider.dart';
-import 'package:movies_app/features/movies/presentation/main_layout/tabs/profile_tab/presentation/cubit/get_history_cubit.dart';
 import 'package:movies_app/features/movies/presentation/main_layout/tabs/profile_tab/presentation/cubit/profile_cubit.dart';
-import 'package:movies_app/features/movies/presentation/main_layout/tabs/profile_tab/presentation/cubit/watchlist_cubit.dart';
-import 'package:movies_app/features/movies/presentation/movie_details/cubit/add_history_cubit.dart';
-import 'package:movies_app/features/movies/presentation/movie_details/cubit/is_watch_list_cubit.dart';
-import 'package:movies_app/features/movies/presentation/movie_details/cubit/movie_details_cubit.dart';
-import 'package:movies_app/features/movies/presentation/movie_details/cubit/movie_suggestions_cubit.dart';
 import 'package:provider/provider.dart';
 import 'config/theme_manager.dart';
 import 'core/resources/routes_manager.dart';
@@ -36,19 +29,9 @@ void main() async {
         ChangeNotifierProvider(create: (_) => AuthProvider()),
         BlocProvider(create: (_) => serviceLocator.get<AuthCubit>()),
         BlocProvider(create: (_) => serviceLocator.get<ProfileCubit>()),
-        BlocProvider(create: (_) => serviceLocator.get<WatchListCubit>()),
-        BlocProvider(create: (_) => serviceLocator.get<GetHistoryCubit>()),
-        BlocProvider(create: (_) => serviceLocator.get<MovieDetailsCubit>()),
-        BlocProvider(
-          create: (_) => serviceLocator.get<MovieSuggestionsCubit>(),
-        ),
-        BlocProvider(create: (_) => serviceLocator<IsWatchListCubit>()),
-        BlocProvider(create: (_) => serviceLocator<AddHistoryCubit>()),
         ChangeNotifierProvider(create: (_) => LangProvider())
       ],
-      child: ChangeNotifierProvider(
-          create: (context)=> MainLayoutProvider(),
-          child: MoviesApp()),
+          child: MoviesApp(),
     ),
   );
 }
@@ -57,6 +40,7 @@ class MoviesApp extends StatelessWidget {
   const MoviesApp({super.key});
   @override
   Widget build(BuildContext context) {
+    var langProvider = Provider.of<LangProvider>(context);
     return ScreenUtilInit(
       designSize: const Size(430, 932),
       minTextAdapt: true,
@@ -64,6 +48,9 @@ class MoviesApp extends StatelessWidget {
       builder: (context, child) => MaterialApp(
         debugShowCheckedModeBanner: false,
         onGenerateRoute: RoutesManager.generateRoute,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        locale: Locale(langProvider.currentLang),
+        supportedLocales: AppLocalizations.supportedLocales,
         initialRoute: RoutesManager.splashScreen,
         theme: ThemeManager.light,
         darkTheme: ThemeManager.dark,
